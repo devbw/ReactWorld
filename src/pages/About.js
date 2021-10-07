d60389a6a96bfb01d2e817b3cc9ed1c53d789e89
devbw <wullaertbrian@gmail.com>
2021-10-08 01:25:34 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <Logo />
+      <h1>A propos</h1>
+      <br/>
+      <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+      <br/>
+    </div>
+  );
+};
+
+export default About;
